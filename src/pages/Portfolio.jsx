6e0b768787787ee186bcbf4abc919754925eacb2
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2023-11-22 18:03:43 -0700

    Added portfolio and resume pages

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,20 +1,22 @@
 export default function Portfolio() {
-    <div>
-    <section id="work" className="headers">Work</section>
-
-    <div id="refactored"><a href="https://ali-aldawoodi.github.io/Limitless-Growth/"
-            alt="man-with-coffee-in-hand">Refactored Code</a></div>
-
-    <div className="top-2">
-        <div className="card-1" id="calculator"><a href="./assets/calculator app.jpg"
-                alt="calculator-image">Calculator</a></div>
-        <div className="card-1" id="one-click-eats"><a href="https://stephen-garcia.github.io/RecipeProject/" alt="page-with-food">One Click Eats</a></div>
-    </div>
-    <div className="bottom-2">
-        <div className="card-2" id="weather-app"><a href="./assets/weather app.jpg" alt="image-of-weather-app">Weather
-                app</a></div>
-        <div className="card-2" id="parking-app"><a href="./assets/parking app.jpg" alt="image-of-parking-app">Parking
-                App</a></div>
-    </div>
-    </div>
-}
+        return (
+                <div>
+                        <section id="work" className="headers">Work</section>
+                        <div className="card-container">
+                                <div id="refactored" ><a href="https://ali-aldawoodi.github.io/Limitless-Growth/"
+                                        alt="man-with-coffee-in-hand">Refactored Code</a></div>
+                        </div>
+                        <div className="top-2">
+                                <div className="card-1" id="calculator"><a href="./assets/calculator app.jpg"
+                                        alt="calculator-image">Calculator</a></div>
+                                <div className="card-1" id="one-click-eats"><a href="https://stephen-garcia.github.io/RecipeProject/" alt="page-with-food">One Click Eats</a></div>
+                        </div>
+                        <div className="bottom-2">
+                                <div className="card-2" id="weather-app"><a href="./assets/weather app.jpg" alt="image-of-weather-app">Weather
+                                        app</a></div>
+                                <div className="card-2" id="parking-app"><a href="./assets/parking app.jpg" alt="image-of-parking-app">Parking
+                                        App</a></div>
+                        </div>
+                </div>
+        )
+};
