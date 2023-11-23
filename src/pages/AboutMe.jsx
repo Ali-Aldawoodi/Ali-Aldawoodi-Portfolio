9f55e54a1ea317c09a542094d4f57cb858591f57
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2023-11-23 12:30:47 -0700

    Trying to fix image

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,15 +1,15 @@
 export default function AboutMe() {
-    return(
+    return (
         <div>
-    <section class="img">
-    <img id="me-image" src="assets\IMG_1364.JPEG" alt="man-with-coffee-in-hand"/>
-</section>
+            <section class="img">
+                <img id="me-image" src="assets\IMG_1364.JPEG" alt="man-with-coffee-in-hand" />
+            </section>
 
-<section id="about-me" class="headers">About Me</section>
-<p id="me"> Hello! My name is Ali Aldawoodi and have a passion for coding. I never had realized it, and for most of
-my life never gave it a shot up until only a few months ago. Well I am very glad that life has lead me to
-this point as I never knew or felt for sure about what purpose and contribution I could make for society.
-Now I do and am very glad to share it with you! </p>
-</div>
-);
+            <section id="about-me" class="headers">About Me</section>
+            <p id="me"> Hello! My name is Ali Aldawoodi and have a passion for coding. I never had realized it, and for most of
+                my life never gave it a shot up until only a few months ago. Well I am very glad that life has lead me to
+                this point as I never knew or felt for sure about what purpose and contribution I could make for society.
+                Now I do and am very glad to share it with you! </p>
+        </div>
+    );
 }
