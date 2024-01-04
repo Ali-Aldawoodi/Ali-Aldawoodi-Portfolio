be04d52bb4133c360c7bcce2e9d9632e4378f12b
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2024-01-03 17:21:57 -0700

    Contact page refactored to simpler code until old logic gets working

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'semantic-ui-css/semantic.min.css';
+
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,57 +1,55 @@
-// export default function Contact() {
-//     return (
-
-//     <div class="parent-footer">
-//         <h3 id="contact-me">Contact Me:</h3>
-//         <div class="child-footer-1"> Email: alialdawoodi99@yahoo.com </div>
-//         <div class="child-footer-2"> Github: https://github.com/Ali-Aldawoodi</div>
-//     </div>
-
-// )};
-
 import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
-export default function MyForm() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+// export default function MyForm() {
+//   const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [message, setMessage] = useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  }
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//   }
 
+export default function Contact () {
   return (
-    <form onSubmit={handleSubmit} className='container border w-25 p-3 mt-5'>
-        <div>
-      <label>Enter your name:
-        <input
-          type="text" 
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-      </div>
-      <div>
-      <label>Enter your email:
-        <input
-          type="text" 
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      </div>
-      <div>
-      <label>Message:
-        <input
-          type="text" 
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-      </label>
-      </div>
-      <input type="submit" />
-    </form>
-  )
-}
+    <div>
+      <h1>Contact me at:</h1>
+      <h2 id='contact-page-email'>Email: alialdawoodi99@yahoo.com</h2>
+      <p id='contact-page-greeting'>Thank you!</p>
+    </div>
+)};
+  
+
+    // <form onSubmit={handleSubmit} className='container border w-25 p-3 mt-5'>
+    //     <div>
+    //   <label>Enter your name:
+    //     <input
+    //       type="text" 
+    //       value={name}
+    //       onChange={(e) => setName(e.target.value)}
+    //     />
+    //   </label>
+    //   </div>
+    //   <div>
+    //   <label>Enter your email:
+    //     <input
+    //       type="text" 
+    //       value={email}
+    //       onChange={(e) => setEmail(e.target.value)}
+    //     />
+    //   </label>
+    //   </div>
+    //   <div>
+    //   <label>Message:
+    //     <input
+    //       type="text" 
+    //       value={message}
+    //       onChange={(e) => setMessage(e.target.value)}
+    //     />
+    //   </label>
+    //   </div>
+    //   <input type="submit" />
+    // </form>
+  
+
 
