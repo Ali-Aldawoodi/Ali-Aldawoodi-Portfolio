70c75f5edbb841d9d0f207ea65cc465ddffab360
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2024-01-05 15:20:46 -0700

    Updated links in portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,7 +3,7 @@ export default function Portfolio() {
                 <div>
                         <section id="work" className="headers mt-2">Projects</section>
                         <div className="card-container">
-                                <div className="card-2" id="league-of-legends"><a href="../assets/parking app.jpg" alt="image-of-league-of-legends-champions">League-of-Legends Cards</a></div>
+                                <div className="card-2" id="league-of-legends"><a href="https://github.com/Richard-Au1/LOL-CARDS" alt="image-of-league-of-legends-champions">League-of-Legends Cards</a></div>
                         </div>
                         <div className="top-2">
                                 <div className="card-1" id="tech-blog"><a href="https://github.com/Ali-Aldawoodi/Tech-blog"
@@ -11,7 +11,7 @@ export default function Portfolio() {
                                 <div className="card-1" id="one-click-eats"><a href="https://stephen-garcia.github.io/RecipeProject/" alt="page-with-food">One Click Eats</a></div>
                         </div>
                         <div className="bottom-2">
-                                <div className="card-2" id="weather-lookup"><a href="../assets/weather app.jpg" alt="image-of-weather-app">Weather Lookup</a></div>
+                                <div className="card-2" id="weather-lookup"><a href="https://ali-aldawoodi.github.io/Weather-lookup-app/" alt="image-of-weather-app">Weather Lookup</a></div>
                                 <div className="card-2" id="calculator" ><a href="https://github.com/Ali-Aldawoodi/Calculator-app"
                                         alt="calculator">Calculator App</a></div>
                         </div>
