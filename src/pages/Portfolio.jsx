eb56ea6a298bd712c902f7b458d817e88264ad56
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2024-01-05 15:17:08 -0700

    Updated portfolio page with correct apps

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,20 +3,19 @@ export default function Portfolio() {
                 <div>
                         <section id="work" className="headers mt-2">Projects</section>
                         <div className="card-container">
-                                <div id="refactored" ><a href="https://ali-aldawoodi.github.io/Limitless-Growth/"
-                                        alt="man-with-coffee-in-hand">Refactored Code</a></div>
+                                <div className="card-2" id="league-of-legends"><a href="../assets/parking app.jpg" alt="image-of-league-of-legends-champions">League-of-Legends Cards</a></div>
                         </div>
                         <div className="top-2">
-                                <div className="card-1" id="tutor-ratings"><a href="https://github.com/Ali-Aldawoodi/Tutor-Ratings"
-                                        alt="Tutor ratings">Tutor-Ratings</a></div>
+                                <div className="card-1" id="tech-blog"><a href="https://github.com/Ali-Aldawoodi/Tech-blog"
+                                        alt="Tutor ratings">Tech-Blog</a></div>
                                 <div className="card-1" id="one-click-eats"><a href="https://stephen-garcia.github.io/RecipeProject/" alt="page-with-food">One Click Eats</a></div>
                         </div>
                         <div className="bottom-2">
-                                <div className="card-2" id="weather-app"><a href="../assets/weather app.jpg" alt="image-of-weather-app">Weather
-                                        app</a></div>
-                                <div className="card-2" id="parking-app"><a href="../assets/parking app.jpg" alt="image-of-parking-app">Parking
-                                        App</a></div>
+                                <div className="card-2" id="weather-lookup"><a href="../assets/weather app.jpg" alt="image-of-weather-app">Weather Lookup</a></div>
+                                <div className="card-2" id="calculator" ><a href="https://github.com/Ali-Aldawoodi/Calculator-app"
+                                        alt="calculator">Calculator App</a></div>
                         </div>
                 </div>
         )
 };
+
